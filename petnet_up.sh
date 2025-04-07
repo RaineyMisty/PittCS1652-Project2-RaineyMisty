a78f7d789760bb6f64cc1f248c87367954367a59
ctx <915800506@qq.com>
2025-04-07 10:21:37 +0800

    change user name(change computer)

--- petnet_up.sh
+++ petnet_up.sh
@@ -1,7 +1,7 @@
 set -x
 
 sudo ip link add name petnet_bridge type bridge
-sudo ip tuntap add dev petnet_tap1 mode tap user cs1652
+sudo ip tuntap add dev petnet_tap1 mode tap user hecate
 sudo ip link set petnet_tap1 master petnet_bridge
 sudo ip addr add 192.168.201.1/24 brd + dev petnet_bridge
 sudo ip link set dev petnet_bridge up
